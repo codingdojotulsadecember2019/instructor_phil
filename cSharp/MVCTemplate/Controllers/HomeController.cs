619a9eebe8ae64d49135678b36df08167d4d281e
Phil Krull <pkrull@codingdojo.com>
2020-02-07 12:59:46 -0600

    add mvc template with models

---
+++ cSharp/MVCTemplate/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCTemplate.Models;
+
+namespace MVCTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            string[] NameArray = new string[]
+            {
+                "Tom",
+                "George",
+                "Sally",
+                "Betsy"
+            };
+
+            return View(NameArray);
+        }
+
+        [HttpPost("users")]
+        public IActionResult AddUser(User UserToAdd)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
